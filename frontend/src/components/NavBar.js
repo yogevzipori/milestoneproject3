13b55c5ba930c5cb83fa4aa50400afed24f573cb
Keoki <keoki.alan.mundon@gmail.com>
2022-07-31 10:01:30 -0700

    added navbar and updated view/editworkout

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function NavBar() {
+  
   return (
-    <div>NavBar</div>
-  )
+    <Nav variant="pills" defaultActiveKey="/home">
+    <Nav.Item>
+      <Nav.Link href="/home">Home</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-1">Start</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-3">Sign out</Nav.Link>
+    </Nav.Item>
+  </Nav>
+  );
 }
--- frontend/src/components/ViewWorkout.js
+++ frontend/src/components/ViewWorkout.js
@@ -1,7 +1,84 @@
 import React from 'react'
-
+import Card from 'react-bootstrap/Card';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 export default function ViewWorkout() {
   return (
-    <div>ViewWorkout</div>
-  )
+    <>
+    <div class="viewworkout">
+    <Card border="primary" style={{ width: '18rem' }}>
+      <Card.Header>Header</Card.Header>
+      <Card.Body>
+        <Card.Title>Primary Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <DropdownButton id="dropdown-item-button" title="Dropdown button">
+      <Dropdown.ItemText>Dropdown item text</Dropdown.ItemText>
+      <Dropdown.Item as="button">Action</Dropdown.Item>
+      <Dropdown.Item as="button">Another action</Dropdown.Item>
+      <Dropdown.Item as="button">Something else</Dropdown.Item>
+    </DropdownButton>
+      </Card.Body>
+    </Card>
+    <br />
+    <Card border="primary" style={{ width: '18rem' }}>
+      <Card.Header>Header</Card.Header>
+      <Card.Body>
+        <Card.Title>Secondary Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <br />
+    <Card border="primary" style={{ width: '18rem' }}>
+      <Card.Header>Header</Card.Header>
+      <Card.Body>
+        <Card.Title>Success Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <br />
+    <Card border="primary" style={{ width: '18rem' }}>
+      <Card.Header>Header</Card.Header>
+      <Card.Body>
+        <Card.Title>Danger Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <br />
+    <Card border="primary" style={{ width: '18rem' }}>
+      <Card.Header>Header</Card.Header>
+      <Card.Body>
+        <Card.Title>Warning Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <br />
+    <Card border="primary" style={{ width: '18rem' }}>
+      <Card.Header>Header</Card.Header>
+      <Card.Body>
+        <Card.Title>Info Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <br />
+    </div>
+  </>
+);
 }
