7fac4e519e5da425ca246c970ed128afadf30e41
Keoki <keoki.alan.mundon@gmail.com>
2022-08-03 07:40:39 -0700

    attempting to fix deployment

--- backend/middleware/defineCurrentUser.js
+++ backend/middleware/defineCurrentUser.js
@@ -4,7 +4,6 @@ const jwt = require("json-web-token");
 async function defineCurrentUser(req, res, next) {
     try {
         if (req.headers.authorization) {
-            console.log(req.headers.authorization)
             const [ method, token ] = req.headers.authorization.split(' ')
                 if( method == 'Bearer' ){
                     const result = await jwt.decode(process.env.JWT_SECRET, token)
